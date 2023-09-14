99a72d01d6b1c6b82f95b7ab1de69aeb1eabd6de
Icedreck101 <nyamwezi.j@gmail.com>
2023-09-14 05:29:21 -0700

    update6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,43 +1,27 @@
 #include "main.h"
 
-
 /**
-
- * print_line - draws a straight line in the terminal
-
- * @n: number of times the character _ should be printed
-
- */
-
+*print_line - Prints a line of underscores.
+*@n: Number of times the character '_' should be printed.
+*
+*Return: void (no return value).
+*/
 void print_line(int n)
-
 {
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i;
 
-        if (n <= 0)
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
 
-        {
-
-                _putchar('\n');
-
-        } else
-
-        {
-
-                int i;
-
-
-                for (i = 1; i <= n; i++)
-
-                {
-
-                        _putchar('_');
-
-                }
-
-                _putchar('\n');
-
-        }
-
-
+_putchar('\n');
+}
 }
 
