556f4bfed27e477c75c47b972f042d018d4c5df5
Icedreck101 <nyamwezi.j@gmail.com>
2023-09-25 02:33:47 -0700

    5th commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,24 +9,37 @@
 *         the string 'haystack', or NULL if the substring is not found.
 */
 char *_strstr(char *haystack, char *needle)
+
 {
-while (*haystack != '\0')
+
+for (; *haystack != '\0'; haystack++)
+
 {
+
 char *l = haystack;
+
 char *p = needle;
 
+
 while (*l == *p && *p != '\0')
+
 {
+
 l++;
+
 p++;
+
 }
+
 
 if (*p == '\0')
+
 return (haystack);
 
-haystack++;
 }
 
-return (NULL);
+
+return (0);
+
 }
 
