3aceb7af7348c18d00ebbfeb151979e1c4ccb358
Icedreck101 <nyamwezi.j@gmail.com>
2023-09-29 02:16:38 -0700

    4th commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
-* main - prints the name of the program
-* @argc: number of arguments
-* @argv: array of arguments
+* main - Entry point of the program
+* @argc: The number of arguments passed
+* @argv: An array of pointers to the arguments
 *
 * Return: Always 0 (Success)
 */
