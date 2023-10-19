fbc66a0aa2b9591179c6c9909e47ab27b2e4a587
Icedreck101 <nyamwezi.j@gmail.com>
2023-10-19 02:12:24 -0700

    #task 101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,29 +1,10 @@
-section .data
-    hello db "Hello, Holberton",0
-    format db "%s",10,0
-
-section .text
-    global _start
-
-_start:
-    ; Push arguments for printf
-    push rdi
-    push rsi
-
-    ; Set rdi to the format string
-    mov rdi, format
-
-    ; Set rsi to the address of hello
-    mov rsi, hello
-
-    ; Call printf
-    call printf
-
-    ; Clean up the stack
-    add rsp, 16
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; status: 0
-    syscall
+       global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
