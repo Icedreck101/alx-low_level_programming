ef72ed0f3c097dfafca074418bdce01b1127c3a9
Icedreck101 <nyamwezi.j@gmail.com>
2023-10-19 04:12:30 -0700

    #3 task commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,30 +1,22 @@
 #include "lists.h"
 
 /**
-* add_node_end - Adds a new node at the end of a linked list.
-* @head: Double pointer to the list_t list.
-* @str: String to put in the new node.
-* Return: Address of the new element, or NULL if it failed.
+* add_nodeint_end - adds a node at the end of a linked list
+* @head: pointer to the first element in the list
+* @n: data to insert in the new element
+*
+* Return: pointer to the new node, or NULL if it fails
 */
-list_t *add_node_end(list_t **head, const char *str)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-if (!head || !str)
-return (NULL);
+listint_t *new_node;
+listint_t *temp = *head;
 
-unsigned int len = strlen(str);
-
-list_t *new_node = malloc(sizeof(list_t));
+new_node = malloc(sizeof(listint_t));
 if (!new_node)
 return (NULL);
 
-new_node->str = strdup(str);
-if (!new_node->str)
-{
-free(new_node);
-return (NULL);
-}
-
-new_node->len = len;
+new_node->n = n;
 new_node->next = NULL;
 
 if (*head == NULL)
@@ -33,11 +25,8 @@ if (*head == NULL)
 return (new_node);
 }
 
-list_t *temp = *head;
 while (temp->next)
-{
 temp = temp->next;
-}
 
 temp->next = new_node;
 
