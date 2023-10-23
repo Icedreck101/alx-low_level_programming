6e37840e8e1b36beae53f9dece114c0adcc6e37d
Icedreck101 <nyamwezi.j@gmail.com>
2023-10-23 09:59:09 -0700

    #task 5 commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,38 +6,16 @@
 */
 void free_listint2(listint_t **head)
 {
-listint_t *current;
+listint_t *temp;
 
-if (head == NULL || *head == NULL)
+if (head == NULL)
 return;
 
 while (*head)
 {
-current = (*head)->next;
+temp = (*head)->next;
 free(*head);
-*head = current;
-}
-
-*head = NULL;
-}
-#include "lists.h"
-
-/**
-* free_listint2 - frees a linked list
-* @head: pointer to the listint_t list to be freed
-*/
-void free_listint2(listint_t **head)
-{
-listint_t *current;
-
-if (head == NULL || *head == NULL)
-return;
-
-while (*head)
-{
-current = (*head)->next;
-free(*head);
-*head = current;
+*head = temp;
 }
 
 *head = NULL;
